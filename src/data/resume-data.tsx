0c7dda5c9839e5aa6333050d7428f641aeab6ef4
Lorenzo Germini <lorenzo.germini@icloud.com>
2024-01-14 22:38:34 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,116 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Lorenzo Germini",
+  initials: "LG",
+  location: "Lille, France, CET",
+  locationLink: "https://www.google.com/maps/place/Lille",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Multidisciplinary Data Scientist passionate about making an impact for patients",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "Wide breadth of knowledge in Life Sciences with a focus in the pharmaceutical sector, complemented by expertise in Data Science (Python) and Machine Learning. My interdisciplinary background and analytical acumen have enabled me to leverage complex data, working collaboratively with teams to address real-world challenges effectively in both pharmaceutical and startup environments. Driven by insatiable curiosity and a passion for learning, I continuously improve my skillset through dedicated self-development. I spend my free time between sports, exciting personal projects and voraciously consuming content on tech entrepreneurship, personal finance, and biohacking topics.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/48798069?v=4",
+  personalWebsiteUrl: "https://lorger.me",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "lorenzo.germini@icloud.com",
+    tel: "+393279220232",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/Blaise-g",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/lorenzogermini/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://twitter.com/spleenboi_",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Polytechnic of Turin",
+      degree: "Bachelor's Degree in Electrical Engineering",
+      start: "2017",
+      end: "2020",
+    },
+    {
+      school: "Swiss Federal Institute of Technology Lausanne (EPFL)",
+      degree: "MSc in Life Sciences Engineering (specialization in Neuroscience & Neuroengingeering)",
+      start: "2020",
+      end: "2022",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      company: "GSK",
+      link: "https://www.gsk.com/en-gb/home/",
+      badges: ["Lille, France"],
+      title: "Data Scientist (Engineering FLP)",
+      logo: GSKLogo,
+      start: "Sep 2023",
+      end: "Present",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Spearheaded the analysis of sensor data from various clean utilities equipment, significantly streamlining the monitoring of clean water consumption. Actively involved in defining and managing the implementation strategy for the most efficient water rejection solutions, in collaboration with key stakeholders. Additionally, I am developing a user-friendly dashboard for service technicians, aimed at enhancing real-time monitoring and proactive maintenance of clean utilities equipment.",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      company: "Liceo Scientifico Cuneo",
+      link: "https://liceocuneo.it/",
+      badges: ["Cuneo, Italy"],
+      title: "Math High School Teacher",
+      logo: LiceoLogo,
+      start: "Dec 2022",
+      end: "Jul 2023",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "After completing my MSc, I took the opportunity to pause and recharge while making an impact. I returned to my roots at my high school alma mater, teaching Math and Physics to high school students. Imparting my graduate school knowledge to inspire the next generation was tremendously rewarding. This experience allowed me to reconnect with my hometown community and share my passion for STEM before embarking on the next chapter of my career. This gap period gave me perspective and renewed purpose.",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      company: "Burgeon Labs",
+      link: "https://www.burgeonlabs.com/",
+      badges: ["Geneva, Switzerland"],
+      title: "Engineering Intern (EPFL Master Thesis Project)",
+      logo: BLLogo,
+      start: "Feb 2022",
+      end: "Oct 2022",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+        "My master thesis investigates how a Natural Language Processing powered tool could be implemented to research the vast scientific literature on topics of longevity and human health and automatically extract evidence-based takeaways from relevant papers according to a selected level of conciseness. Beyond working on my thesis, I supported the early development of the company’s first digital health product alongside the CEO with a focus on product management and quantitative research.",
     },
     {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
-    },
+      company: "Roche",
+      link: "https://www.roche.com/",
+      badges: ["Basel, Switzerland"],
+      title: "Internship Programme 'Think Tank in Innovation & Sustainability'",
+      logo: RocheLogo,
+      start: "Jul 2021",
+      end: "Oct 2021",
+      description: "Oversaw the development of a customized filling machine prototype to supply drugs to patients based on individual demand. Performed hardware-software integration testing with a data-acquisition platform and facilitated fruitful incorporation of outside partners (start-ups, companies) into workflows. Within the same framework, I designed a Machine Learning model for accurate fill parameter determination in the context of Personalized Medicine solutions for drugs in the pipeline. Validating my results with various departments as well as delivering pitches to senior executives have enabled me to foster a culture of curiosity and collaboration while solving for real-world challenges in the Pharma Industry.",
+    }
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Applied Data Analytics",
+    "Python",
+    "Power BI",
+    "Machine Learning",
+    "Pharmaceutical Manufacturing",
+    "Effective Communication",
+    "Strong Analytical Skills",
   ],
   projects: [
     {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "Anki Buddy",
+      techStack: ["Side Project", "Generative AI"],
       description:
-        "Browser extension that records everything happening in a web application",
+        "Custom GPT for seamless memory flashcards creation to maximise learning retention.",
       logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "Anki Buddy",
+        href: "https://chat.openai.com/g/g-I56djVcjg-anki-buddy",
       },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
