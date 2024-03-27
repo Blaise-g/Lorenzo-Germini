fc60837e5e2936124c504e39f8ffadcfe395ee3d
Lorenzo Germini <lorenzo.germini@icloud.com>
2024-03-27 18:13:47 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -25,7 +25,7 @@ export const RESUME_DATA = {
   location: "Lille, France, CET",
   locationLink: "https://www.google.com/maps/place/Lille",
   about:
-    "Data Scientist with expertise in pharma and healthcare, passionate about leveraging data-driven insights to solve complex problems and create meaningful impact on people's lives and the environment.",
+    "Data Scientist who enjoys solving complex problems to create meaningful impact on people's lives and the environment.",
   summary:
     "Wide breadth of knowledge in Life Sciences with a focus on Pharmaceutical Manufacturing & Digital Health, complemented by expertise in Data Science (Python) and Machine Learning. My interdisciplinary background and analytical acumen have enabled me to leverage complex data, working collaboratively with teams to tackle real-world technical challenges in both pharmaceutical and startup environments. Driven by insatiable curiosity and a passion for learning, I continuously improve my skillset through dedicated self-development. I spend my free time engaged in sports, exciting personal projects in the Generative AI space, and voraciously consuming content on tech entrepreneurship, personal finance, and biohacking topics.",
   avatarUrl: "https://avatars.githubusercontent.com/u/48798069?v=4",
