52a803458d87765667449fc77beed734da8d130a
Lorenzo Germini <lorenzo.germini@icloud.com>
2024-06-16 12:02:06 +0200

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -28,7 +28,7 @@ export const RESUME_DATA: ResumeData = {
   about:
     "Data Scientist who enjoys solving complex problems to create meaningful impact on people's lives and the environment",
   summary:
-    "Wide breadth of knowledge in Life Sciences with a focus on Pharmaceutical Manufacturing & Digital Health, complemented by expertise in Data Science (Python) and Machine Learning. My interdisciplinary background and analytical acumen have enabled me to leverage complex data, working collaboratively with teams to tackle real-world technical challenges in both pharmaceutical and startup environments. Driven by insatiable curiosity and a passion for learning, I continuously improve my skillset through dedicated self-development. I spend my free time engaged in sports, exciting personal projects, and voraciously consuming content on tech entrepreneurship, personal finance, and biohacking topics.",
+    "Results-driven Data Scientist with a solid foundation in Data Science, Analytics, and Machine Learning. My interdisciplinary background and analytical acumen make me adept at leveraging complex data to identify trends, optimize processes, and tackle real-world business challenges. I have demonstrated my ability to collaborate with cross-functional teams to deliver data-driven insights and enhance operational efficiency in fast-paced environments in both Pharma and Startup sectors. Passionate about making meaningful impacts on people's lives through technology and driven by insatiable curiosity, I continuously strive to improve my skillset and contribute to innovative projects. In my free time, I engage in sports, work on personal projects, and voraciously consume content on tech entrepreneurship, personal finance, and biohacking.",
   avatarUrl: "https://avatars.githubusercontent.com/u/48798069?v=4",
   personalWebsiteUrl: "https://lorenzo-germini.vercel.app/",
   contact: {
@@ -76,10 +76,10 @@ export const RESUME_DATA: ResumeData = {
       start: "Sep 2023",
       end: "Present",
       description: [
-        "Led analysis of sensor data from various sources using Python and PowerBi to enhance water consumption monitoring, identifying inefficiencies, and initiating improvement projects.",
-        "Designed and managed strategies for water rejection solutions in clean utilities, focusing on Distillators and WFI user levels, aiming to save 10,000 m³ annually through hardware upgrades and real-time software optimization.",
-        "Created a Streamlit analytics dashboard for clean utilities equipment to monitor KPIs, enabling proactive maintenance and reducing reliance on external companies, saving costs and cutting manual data inspection time by 50%.",
-        "Exploring Generative AI-powered features in the dashboard for automatic trend presentation and interactive data exploration.",
+        "Conducted comprehensive analysis of sensor data using Python and PowerBi, leading to significant improvements in water consumption monitoring and the initiation of impactful improvement projects.",
+        "Developed and implemented strategies for optimizing water rejection processes in the Clean Utilities, resulting in an annual savings of 10,000 m³ through innovative hardware upgrades and real-time software solutions.",
+        "Designed and deployed a Streamlit analytics dashboard to monitor key performance indicators (KPIs), facilitating proactive maintenance and reducing dependency on external companies, cutting costs and achieving a 50% reduction in manual data inspection time.",
+        "Investigated and integrated Generative AI features to enhance data trend analysis and interactive exploration, improving data-driven decision-making.",
       ],
     },
     {
@@ -91,8 +91,9 @@ export const RESUME_DATA: ResumeData = {
       start: "Dec 2022",
       end: "Aug 2023",
       description: [
-        "Taught Math and Physics at my high-school alma mater, reconnecting with my hometown community. Shared my passion for STEM and fostered a renewed sense of purpose before the next chapter of my career.",
+        "Taught Math and Physics at my high-school alma mater, reconnecting with my hometown community and fostering a passion for STEM subjects in the next generation of students.",
         "Developed and tested a Minimum Viable Product using Stable Diffusion for generating interior designs from empty space images. Partnered with local businesses to understand user preferences and market trends, leading to the project's closure due to limited interest.",
+        "Demonstrated versatility and entrepreneurial spirit by wearing multiple hats in a fast-paced environment, balancing technical development with market research and user engagement.",
       ],
     },
     {
@@ -105,7 +106,8 @@ export const RESUME_DATA: ResumeData = {
       end: "Oct 2022",
       description: [
         "Developed a Generative AI tool for researching scientific literature on longevity and human health, extracting evidence-based summaries in TLDR or extended format.",
-        "Collaborated with the CEO to define product management strategies and streamline business processes for the company's first digital health product, leading to the establishment of a partnership with a renowned clinic.",
+        "Worked closely with the CEO to refine product management strategies and enhance business processes, contributing to the successful launch of a digital health product and securing a key partnership with a renowned clinic.",
+        "Actively engaged with various business functions to understand user needs and drive impactful solutions, demonstrating a strong commitment to improving health outcomes through technology.",
       ],
     },
     {
@@ -117,23 +119,23 @@ export const RESUME_DATA: ResumeData = {
       start: "Jul 2021",
       end: "Oct 2021",
       description: [
-        "Managed development of customized filling machine prototype for personalized drug supply, including hardware-software integration testing and collaboration with external partners.",
-        "Improved tuning of filling prototype by creating a Machine Learning model (gradient-boosted tree) to optimize fill parameter determination for Personalized Medicine solutions, reducing tuning time by 20% compared to manual methods used in other departments.",
+        "Led the development of a customized filling machine prototype for personalized drug supply, coordinating hardware-software integration and collaborating with external partners to ensure project success.",
+        "Enhanced the tuning process of the filling prototype by developing a gradient-boosted tree machine learning model, optimizing fill parameters and reducing tuning time by 20% compared to manual methods.",
+        "Emphasized the practical applications of machine learning to drive innovation in personalized medicine, showcasing my ability to translate technical expertise into real-world impact.",
       ],
     },
   ],
   skills: [
     "Data Science",
     "Python",
+    "SQL",
     "Machine Learning",
     "Generative AI",
     "Data Analytics",
-    "PyTorch",
-    "SQL",
     "Microsoft Power BI",
     "Digital Health",
-    "Pharmaceutical Manufacturing",
-    "Life Sciences",
+    "Cross-Functional Collaboration",
+    "Startup Experience",
   ],
   projects: [
     {
